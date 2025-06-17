84740dd641ad7b98dda63fe172bb4b481bc586c2
BinaryDigit <binarydigit@fastmail.com>
2025-06-17 06:26:41 -0400

    Quartz sync: Jun 17, 2025, 6:26 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "",
     },
     locale: "en-US",
-    baseUrl: "garden.liz.computer",
+    baseUrl: "binarydigitlol.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
