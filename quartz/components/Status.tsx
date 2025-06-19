ae77e130f2ce18bc26c6c56c12f1d80734db15c1
BinaryDigit <binarydigit@fastmail.com>
2025-06-19 18:24:38 -0400

    Quartz sync: Jun 19, 2025, 6:24 PM

--- quartz/components/Status.tsx
+++ quartz/components/Status.tsx
@@ -3,7 +3,7 @@ export default (() => {
     // return <p><h3>Current Status:</h3><div id="statuscafe"><div id="statuscafe-username"></div><div id="statuscafe-content"><script src="https://status.cafe/current-status.js?name=lizbytes" defer></script></div>
     // </div>
     // </p>    
-    return <p><script src="https://status.lol/binarydigit.js?time&fluent&pretty"></script></p>
+    return <p><script src="https://status.lol/binarydigit.js?time&pretty&fluent"></script></p>
   }
 
   return Status
